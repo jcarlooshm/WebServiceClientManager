a768858b0ef15498bdf648176c9df351f8a6359a
Juan Carlos Hernández Martínez <jhernandezm@aeropuertosgap.com.mx>
2023-11-06 17:56:29 -0600

    feat: se agrega modelo ClientResponse

---
+++ WebServiceClientManager/Models/ClientResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace WebServiceClientManager.Models
+{
+    public class ClientResponse<TResponse>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public TResponse Content { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
