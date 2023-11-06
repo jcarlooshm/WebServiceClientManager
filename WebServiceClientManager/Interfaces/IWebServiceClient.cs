cbc0eb5dc25eb43e5d40d1a2afd1b18edf7240b4
Juan Carlos Hernández Martínez <jhernandezm@aeropuertosgap.com.mx>
2023-11-06 17:58:11 -0600

    feat: se agrega interfaz IWebServiceClient

---
+++ WebServiceClientManager/Interfaces/IWebServiceClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebServiceClientManager.Enums;
+using WebServiceClientManager.Models;
+
+namespace WebServiceClientManager.Interfaces
+{
+    public interface IWebServiceClient
+    {
+        event Action<HttpResponseMessage> UnauthorizedResponseReceived;
+        event Action<HttpResponseMessage> UnauthorizedRetriedResponseReceived;
+
+        string CreateQueryParameters(object parametros);
+        ClientResponse<TResponse> Delete<TResponse>(string endpoint);
+        Task<ClientResponse<TResponse>> DeleteAsync<TResponse>(string endpoint);
+        ClientResponse<TResponse> Get<TResponse>(string endpoint);
+        Task<ClientResponse<TResponse>> GetAsync<TResponse>(string endpoint);
+        ClientResponse<TResponse> Patch<TResponse>(string endpoint, object content, EContentType contentType);
+        Task<ClientResponse<TResponse>> PatchAsync<TResponse>(string endpoint, object content, EContentType contentType);
+        ClientResponse<TResponse> Post<TResponse>(string endpoint, object content, EContentType contentType);
+        Task<ClientResponse<TResponse>> PostAsync<TResponse>(string endpoint, object content, EContentType contentType);
+        ClientResponse<TResponse> Put<TResponse>(string endpoint, object content, EContentType contentType);
+        Task<ClientResponse<TResponse>> PutAsync<TResponse>(string endpoint, object content, EContentType contentType);
+        void SetAuthorizationToken(string authorizationToken);
+        void SetTokenRefreshFunc(Func<Task<string>> refreshFunc);
+    }
+}
