d8f0b366c43ba03d362523c1674c3adb2ab2de3e
Juan Carlos Hernández Martínez <jhernandezm@aeropuertosgap.com.mx>
2023-11-06 17:54:39 -0600

    feat: se agrega enumerador EContentType

---
+++ WebServiceClientManager/Enums/EContentType.cs
@@ -0,0 +1,9 @@
+﻿namespace WebServiceClientManager.Enums
+{
+    public enum EContentType
+    {
+        application_json = 0,
+        multipart_form_data = 1,
+        application_x_www_form_urlencoded = 2
+    }
+}
